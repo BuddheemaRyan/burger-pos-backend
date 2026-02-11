f004d09b34b5b3eeb2e99dc9b4e78920cf1b2ef2
BuddheemaRyan <buddimaraya16@gmail.com>
2026-02-11 14:06:13 +0530

    completed Product Entity

---
+++ burger/src/main/java/com/example/burger/model/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.burger.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private double price;
+    private String image;
+
+}
