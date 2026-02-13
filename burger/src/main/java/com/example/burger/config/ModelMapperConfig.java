0fd4c64019fcac20324774aae63c4619de724fcb
BuddheemaRyan <buddimaraya16@gmail.com>
2026-02-13 15:36:31 +0530

    completed modelmapperconfig

--- burger/src/main/java/com/example/burger/config/ModelMapperConfig.java
+++ burger/src/main/java/com/example/burger/config/ModelMapperConfig.java
@@ -1,4 +1,14 @@
 package com.example.burger.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return  modelMapper;
+    }
 }
