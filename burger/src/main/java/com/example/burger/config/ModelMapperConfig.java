66e350fa7be716512fe9386f0daa4aba63787e1b
BuddheemaRyan <buddimaraya16@gmail.com>
2026-02-13 15:31:10 +0530

    created modelmappercofig file

---
+++ burger/src/main/java/com/example/burger/config/ModelMapperConfig.java
@@ -0,0 +1,4 @@
+package com.example.burger.config;
+
+public class ModelMapperConfig {
+}
--- burger/src/main/java/com/example/burger/controller/ProductController.java
+++ burger/src/main/java/com/example/burger/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.burger.controller;
 
 
+import com.example.burger.model.dto.ProductDto;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
